9f53c6a7b87b005ac582a54baa7873b1b43ec9a8
ph3b <mathiasiden@me.com>
2014-10-10 20:24:43 +0200

    Implementert date
    Fikset date-feltet. Konverterer tiden riktig.

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -15,13 +15,45 @@ angular.module('kaffeAppApp')
     });
 
     $scope.addPost = function(){
+    	$scope.datepost.datetime = adjustTime($scope.datepost.datetime);
     	datepost.addNewDatePost($scope.datepost).then(function(response){
     		datepost.getDatePosts().then(function(dateposts){
     			$scope.dateposts = dateposts; 		
     		});
     	});
+  		
+  		console.log($scope.datepost.datetime)
     }
     $scope.facebookProfilePic = function(userid){
-    	return 'https://graph.facebook.com/' + userid + '/picture?height=60'
+    	return 'https://graph.facebook.com/' + userid + '/picture?height=120'
     }
+    var adjustTime = function(arg){
+    	var today = new Date();
+  		var d = new Date(arg);
+  		d.setFullYear(today.getFullYear());
+  		d.setMonth(today.getMonth());
+  		d.setUTCDate(today.getUTCDate());
+  		d.setSeconds(0);
+  		return d;
+    }
+    $scope.timeOffset = function(time){
+    	var d = new Date(time)
+    	var hour = "";
+    	var minutes = "";
+    	if(d.getHours() < 10){
+    		hour = '0' + d.getHours();
+    	}
+    	if(d.getHours() > 10){
+    		hour = d.getHours();
+    	}
+    	if(d.getMinutes() < 10){
+    		minutes = '0' + d.getMinutes();
+    	}
+    	if(d.getMinutes() > 10){
+    		minutes = d.getMinutes();
+    	}
+
+    	return hour + ':' + minutes;
+    }
+
   });
